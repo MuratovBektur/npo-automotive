37f14e61b8183164f05da8aea807367d818ea985
Bektur Muratov <prostreyd@gmail.com>
2025-11-21 17:09:19 +0600

    first commit

---
+++ client/vite.config.ts
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true
+    },
+    port: 3000,
+    host: true,
+    strictPort: true
+  },
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
